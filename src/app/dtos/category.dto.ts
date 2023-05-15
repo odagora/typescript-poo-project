d08d0b8f375ae69b5de27c8ed6e2066a5fd9f05c
Daniel <oscar.d.gonzalez@gmail.com>
2023-05-14 21:50:47 -0500

    :recycle: Refactor category dto with constructor

--- src/app/dtos/category.dto.ts
+++ src/app/dtos/category.dto.ts
@@ -6,22 +6,30 @@ export interface ICreateCategoryDto extends Omit<Category, 'id'>{}
 export class CreateCategoryDto implements ICreateCategoryDto {
   @IsNotEmpty()
   @Length(4, 140)
-  name!: string;
+  name: string;
 
   @IsUrl()
   @IsNotEmpty()
-  image!: string;
+  image: string;
 
   @IsOptional()
   @IsEnum(AccessType)
   access?: AccessType | undefined;
+
+  constructor(
+    name: string,
+    image: string,
+    access?: AccessType
+  ){
+    this.name = name
+    this.image = image
+    this.access = access
+  }
 }
 
 (async () => {
   try {
-    const dto = new CreateCategoryDto()
-    dto.name ='Daniel'
-    dto.image = 'https://api.escuelajs.co/api/v1/products'
+    const dto = new CreateCategoryDto('Dan', 'https://api.escuelajs.co/api/v1/products')
     await validateOrReject(dto)
   } catch (error) {
     console.log(error);
